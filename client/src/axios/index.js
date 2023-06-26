b9abff57022aafb2d3e3eedaa73fcd2f1d723a91
Fevzi Atanoğlu <fevziatan2@gmail.com>
2023-06-26 09:37:31 +0300

    Axios Update

---
+++ client/src/axios/index.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const HTTP = axios.create({
+    baseURL : "http://localhost:4000"
+});
+
+
+// user functions
+export async function login(formData){
+    return await HTTP.post("/users/login" , formData);
+}
+
+export async function register(formData){
+    return await HTTP.post("/users/register" , formData);
+}
+
+export async function getFolders(id){
+    return await HTTP.get(`/users/getFolders/${id}`);
+}
+
+// Folders functions
+export async function addFolder(formData){
+    return await HTTP.post("/folders/add" , formData);
+}
+
+export async function updateFolder(formData){
+    return await HTTP.put("/folders/update" , formData);
+}
+
+export async function getCards(folderId){
+    return await HTTP.get(`/users/getFolders/${folderId}`);
+}
+
+export async function removeFolder(folderId){
+    return await HTTP.delete(`/folders/remove/${folderId}`);
+}
+
+// Card functions
+
+export async function addCard(formData){
+    return await HTTP.post("/cards/add" , formData);
+}
+
+export async function updateCard(formData){
+    return await HTTP.post("/cards/update" , formData);
+}
+
+
